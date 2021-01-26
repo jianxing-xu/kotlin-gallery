b0d31df039fdaaf2d5a2595faac563fabe46d2c9
xujianxing <xjx_me@yeah.net>
2021-01-26 10:47:45 +0800

    modify: GalleryViewModel

--- app/src/main/java/cn/xu/gallery/GalleryViewModel.kt
+++ app/src/main/java/cn/xu/gallery/GalleryViewModel.kt
@@ -24,15 +24,19 @@ class GalleryViewModel(application: Application) : AndroidViewModel(application)
     // 获取数据
     fun fetchData() {
         // 构建 StringRequest
-        StringRequest(
-            Request.Method.GET,
+        object : StringRequest(
+            Method.GET,
             getUrl(),
             { _photoList.value = Gson().fromJson(it, Pixabay::class.java).hits.toList() },
             {
                 Toast.makeText(getApplication(), "请求错误", Toast.LENGTH_LONG).show()
                 Log.d(TAG, "fetchData: $it")
             }
-        ).also {
+        ) {
+            override fun getHeaders(): MutableMap<String, String> {
+                return mutableMapOf("Authorization" to "563492ad6f91700001000001b801c7451e82434d924781e2bc999f1a")
+            }
+        }.also {
             // 添加到请求队列
             VolleySingleton.create(getApplication()).requestQueue.add(it);
         }
@@ -44,6 +48,7 @@ class GalleryViewModel(application: Application) : AndroidViewModel(application)
 
     // 随机构造查询关键字Url
     private fun getUrl(): String {
-        return "https://pixabay.com/api/?key=12472743-874dc01dadd26dc44e0801d61&q=${keyWords.random()}&per_page=100";
+        return "https://api.pexels.com/v1/search?query=${keyWords.random()}&per_page=${100}&page=${1}";
     }
 }
+
